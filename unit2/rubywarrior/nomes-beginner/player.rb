e77970fb2e5823dc44b56be70e63adad7cd7c440
Naomi Plasterer <nplaster@mines.edu>
2014-01-18 12:56:29 -0700

    passed level 6 a very unclean code. need to refactor

--- unit2/rubywarrior/nomes-beginner/player.rb
+++ unit2/rubywarrior/nomes-beginner/player.rb
@@ -1,27 +1,40 @@
 #Naomi Plasterer
-#Level 4 used instance variables to store previous health
+#used instance variables to store previous health
+#Used constant to store the max health
+#used class variable for refrencing forward or backwards
+#used functions for repative code like checking health etc
+#used hash
+#used array used to store previously visited places
 
 class Player
+  @health = nil
+  @@wall = false
+  @@attacktime = 0
   def play_turn(warrior)
-    
-    if (warrior.health == 20)
-      if (warrior.feel.empty?)
+    if (@@wall == false)
+      if (warrior.feel(:backward).empty?)
+        warrior.walk!(:backward)
+      elsif (warrior.feel(:backward).wall?)
+        @@wall = true
         warrior.walk!
       else
-        if (warrior.feel.captive?)
-          warrior.rescue!
+        if (warrior.feel(:backward).captive?)
+          warrior.rescue!(:backward)
         else
           warrior.attack!
         end
       end
-    @health=warrior.health
     else
       if (warrior.feel.empty?)
-        if (warrior.health < @health)
-          warrior.walk!
-        else
+        if(warrior.health < @health && @@attacktime < 4)
+          warrior.walk!(:backward)
+        elsif(warrior.health != 20 && warrior.health>=@health)
           warrior.rest!
-        end 
+        else
+          warrior.walk!
+          @@attacktime = @@attacktime + 1
+          
+        end
       else
         if (warrior.health < @health)
           if (warrior.feel.captive?)
@@ -31,10 +44,9 @@ class Player
           end
         else
           warrior.rest!
-        end 
+        end
       end
-      @health=warrior.health
     end
-    
+  @health = warrior.health 
   end
 end
