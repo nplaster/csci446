8ebf060153ef58b75c062d520980a30e192872a9
Naomi Plasterer <nplaster@mines.edu>
2014-01-26 00:33:45 -0700

    added store feature and styled front page

--- unit4/shelter/config/routes.rb
+++ unit4/shelter/config/routes.rb
@@ -1,4 +1,5 @@
 Shelter::Application.routes.draw do
+  get "store/index"
   resources :animals
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +7,7 @@ Shelter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
