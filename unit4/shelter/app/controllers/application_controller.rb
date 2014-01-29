6c14ca246c7137b8e76ab441e69c2000f78376f1
Naomi Plasterer <nplaster@mines.edu>
2014-01-29 15:50:08 -0700

    did a little work on unit5, add capabilities to sort animals for unit4

--- unit4/shelter/app/controllers/application_controller.rb
+++ unit4/shelter/app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
+  helper_method :sort_column, :sort_direction
+  
+  def index
+    @animals = Animal.order(sort_column + " " + sort_direction)
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  private
+  
+  def sort_column
+    Animal.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
--- unit5/depot/app/controllers/line_items_controller.rb
+++ unit5/depot/app/controllers/line_items_controller.rb
@@ -24,7 +24,8 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
