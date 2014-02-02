5dfe85f73fc914fe292b92f98e59814579842555
Naomi Plasterer <nplaster@mines.edu>
2014-02-01 21:05:29 -0700

    finished chapter 10 coding cart can be emptied

--- unit5/depot/test/controllers/line_items_controller_test.rb
+++ unit5/depot/test/controllers/line_items_controller_test.rb
@@ -1,49 +1,90 @@
-require 'test_helper'
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
+class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]
+  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
-class LineItemsControllerTest < ActionController::TestCase
-  setup do
-    @line_item = line_items(:one)
+  # GET /line_items
+  # GET /line_items.json
+  def index
+    @line_items = LineItem.all
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:line_items)
+  # GET /line_items/1
+  # GET /line_items/1.json
+  def show
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
+  # GET /line_items/new
+  def new
+    @line_item = LineItem.new
   end
 
-  test "should create line_item" do
-    assert_difference('LineItem.count') do
-      post :create, line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id }
+  # GET /line_items/1/edit
+  def edit
+  end
+
+  # POST /line_items
+  # POST /line_items.json
+  def create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product.id)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to @line_item.cart,
+          notice: 'Line item was successfully created.' }
+        format.json { render action: 'show',
+          status: :created, location: @line_item }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /line_items/1
+  # PATCH/PUT /line_items/1.json
+  def update
+    respond_to do |format|
+      if @line_item.update(line_item_params)
+        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /line_items/1
+  # DELETE /line_items/1.json
+  def destroy
+    @line_item.destroy
+    respond_to do |format|
+      format.html { redirect_to line_items_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_line_item
+      @line_item = LineItem.find(params[:id])
     end
 
-    assert_redirected_to line_item_path(assigns(:line_item))
-  end
-
-  test "should show line_item" do
-    get :show, id: @line_item
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @line_item
-    assert_response :success
-  end
-
-  test "should update line_item" do
-    patch :update, id: @line_item, line_item: { product_id: @line_item.product_id }
-    assert_redirected_to line_item_path(assigns(:line_item))
-  end
-
-  test "should destroy line_item" do
-    assert_difference('LineItem.count', -1) do
-      delete :destroy, id: @line_item
+    # Never trust parameters from the scary internet, only allow the white
+    # list through.
+    def line_item_params
+      params.require(:line_item).permit(:product_id)
     end
-
-    assert_redirected_to line_items_path
-  end
+  #...
 end
