2000cc8be5ce32f108254adce987b48ee4bf42ff
Naomi Plasterer <nplaster@mines.edu>
2014-02-04 19:36:36 -0700

    generate controller

--- unit5/depot/app/controllers/users_controller.rb
+++ unit5/depot/app/controllers/users_controller.rb
@@ -1,74 +1,59 @@
-class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
+require 'test_helper'
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
+class UsersControllerTest < ActionController::TestCase
+  setup do
+    @user = users(:one)
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
+  test "should get new" do
+    get :new
+    assert_response :success
   end
 
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create, user: { name: 'sam', password: 'secret',
+        password_confirmation: 'secret' }
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
+    assert_redirected_to users_path
+  end
+
+  test "should show user" do
+    get :show, id: @user
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @user
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch :update, id: @user, user: { name: @user.name, password: 'secret',
+        password_confirmation: 'secret' }
+    assert_redirected_to users_path
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @user
     end
+
+    assert_redirected_to users_path
+  end
 end
