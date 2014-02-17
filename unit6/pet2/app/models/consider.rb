0667068106e67eb4f885887b1758f49d75bf7d9f
Naomi Plasterer <nplaster@mines.edu>
2014-02-16 19:32:07 -0700

    got cart like object setup

--- unit6/pet2/app/models/consider.rb
+++ unit6/pet2/app/models/consider.rb
@@ -1,2 +1,3 @@
 class Consider < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
 end
--- unit6/pet2/app/models/line_item.rb
+++ unit6/pet2/app/models/line_item.rb
@@ -1,4 +1,5 @@
 class LineItem < ActiveRecord::Base
   belongs_to :animal
   belongs_to :foster
+  belongs_to :consider
 end
