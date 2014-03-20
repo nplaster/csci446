56827eda76538f42e88637cf275a3f9b29bf6a8e
Naomi Plasterer <nplaster@mines.edu>
2014-03-19 20:07:48 -0600

    finished tutorial for data mapper

--- unit9/books_partII/simple_rack.rb
+++ unit9/books_partII/simple_rack.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 require 'data_mapper'
-require_relative 'book'
+DataMapper.setup(:default, 'sqlite:./books.sqlite3.db')
+require_relative 'Book'
+DataMapper.finalize
 
-get '/' do
+get '/form' do
+	"Hello world"
+end
+
+post '/list' do
+	@books = Book.all
 	"Hello world"
 end
